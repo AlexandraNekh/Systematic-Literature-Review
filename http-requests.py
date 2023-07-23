7509c569eafef3db4ef0175c6b9d26bff34e0078
Aleksandra Nekhviadovich <aliaksandra.nekhviadovich@rwth-aachen.de>
2023-07-24 00:57:03 +0200

    add springer link

--- http-requests.py
+++ http-requests.py
@@ -1,5 +1,6 @@
 import os
 
+############################################### INPUT
 years = ['2000', '2023']
 search_terms = [['Stakeholder involvement', 'Participatory design', 'Stakeholder engagement', 'User-centered design',
                  'HCI', 'Human Computer Interaction', 'Collaborative design', 'Explainable AI', 'Data Transparency',
@@ -7,6 +8,9 @@ search_terms = [['Stakeholder involvement', 'Participatory design', 'Stakeholder
                  'Project management'],
                 ['MLOps', 'DevOps', 'AIOps'],
                 ['Cloud', 'Machine Learning', 'Artificial Intelligence', 'Cloud-based', 'AI-based', 'ML-based']]
+
+
+############################################### IEEE
 
 ieee_url = '"https://ieeexplore.ieee.org/search/searchresult.jsp?action=search&matchBoolean=true&queryText=(('
 
@@ -31,6 +35,8 @@ print("IEEE url: " + ieee_url)
 os.system('start chrome ' + ieee_url)
 
 
+############################################### ACM DL
+
 acm_dl_url = '"https://dl.acm.org/action/doSearch?fillQuickSearch=false&target=advanced&expand=dl&field1=AllField&text1='
 
 add_term = ''
@@ -48,8 +54,32 @@ for i, and_term in enumerate(search_terms):
     add_term = ''
 
 
-acm_dl_url += '&AfterMonth=1&AfterYear=2000&BeforeMonth=12&BeforeYear=2023"'
+acm_dl_url += '&AfterMonth=1&AfterYear=' + years[0] + '&BeforeMonth=12&BeforeYear=' + years[1] + '"'
 
 print("ACM DL url: " + acm_dl_url)
 os.system('start chrome ' + acm_dl_url)
 
+
+############################################### SPRINGER
+
+springer_url = '"https://link.springer.com/search?date-facet-mode=between&facet-start-year=' + years[0] + '&facet-end-year=' + years[1] + '&query='
+
+add_term = ''
+for i, and_term in enumerate(search_terms):
+    if i!=0:
+        add_term += '+AND+'
+    add_term += '%28'
+    for j, or_term in enumerate(and_term):
+        if j!=0:
+            add_term += '+OR+'
+        add_term += or_term.replace(' ', '+')
+
+    add_term += '%29'
+    springer_url += add_term
+    add_term = ''
+
+
+springer_url += '&showAll=true"'
+
+print("Springer url: " + springer_url)
+os.system('start chrome ' + springer_url)
