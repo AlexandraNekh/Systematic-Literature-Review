616475844766707fcafe88c74e32369130a677fe
Aleksandra Nekhviadovich <aliaksandra.nekhviadovich@rwth-aachen.de>
2023-07-24 02:01:44 +0200

    reformat file

--- http-requests.py
+++ http-requests.py
@@ -9,18 +9,17 @@ search_terms = [['Stakeholder involvement', 'Participatory design', 'Stakeholder
                 ['MLOps', 'DevOps', 'AIOps'],
                 ['Cloud', 'Machine Learning', 'Artificial Intelligence', 'Cloud-based', 'AI-based', 'ML-based']]
 
-
 ############################################### IEEE
 
 ieee_url = '"https://ieeexplore.ieee.org/search/searchresult.jsp?action=search&matchBoolean=true&queryText=(('
 
 add_term = ''
 for i, and_term in enumerate(search_terms):
-    if i!=0:
+    if i != 0:
         add_term += '%20AND%20'
     add_term += '('
     for j, or_term in enumerate(and_term):
-        if j!=0:
+        if j != 0:
             add_term += '%20OR%20'
         add_term += '%22All%20Metadata%22:' + or_term.replace(' ', '%20')
 
@@ -28,12 +27,12 @@ for i, and_term in enumerate(search_terms):
     ieee_url += add_term
     add_term = ''
 
-ieee_url += '))&highlight=true&returnFacets=ALL&returnType=SEARCH&matchPubs=true&ranges=' + years[0] + '_' + years[1] + '_Year&' \
-            'refinements=ContentType:Conferences&refinements=ContentType:Journals&refinements=ContentType:Standards"'
+ieee_url += '))&highlight=true&returnFacets=ALL&returnType=SEARCH&matchPubs=true&ranges=' + years[0] + '_' + years[
+    1] + '_Year&' \
+         'refinements=ContentType:Conferences&refinements=ContentType:Journals&refinements=ContentType:Standards"'
 
 print("IEEE url: " + ieee_url)
 os.system('start chrome ' + ieee_url)
-
 
 ############################################### ACM DL
 
@@ -41,11 +40,11 @@ acm_dl_url = '"https://dl.acm.org/action/doSearch?fillQuickSearch=false&target=a
 
 add_term = ''
 for i, and_term in enumerate(search_terms):
-    if i!=0:
+    if i != 0:
         add_term += '+AND+'
     add_term += '%28'
     for j, or_term in enumerate(and_term):
-        if j!=0:
+        if j != 0:
             add_term += '+OR+'
         add_term += or_term.replace(' ', '+')
 
@@ -53,24 +52,23 @@ for i, and_term in enumerate(search_terms):
     acm_dl_url += add_term
     add_term = ''
 
-
 acm_dl_url += '&AfterMonth=1&AfterYear=' + years[0] + '&BeforeMonth=12&BeforeYear=' + years[1] + '"'
 
 print("ACM DL url: " + acm_dl_url)
 os.system('start chrome ' + acm_dl_url)
 
-
 ############################################### SPRINGER
 
-springer_url = '"https://link.springer.com/search?date-facet-mode=between&facet-start-year=' + years[0] + '&facet-end-year=' + years[1] + '&query='
+springer_url = '"https://link.springer.com/search?date-facet-mode=between&facet-start-year=' + years[
+    0] + '&facet-end-year=' + years[1] + '&query='
 
 add_term = ''
 for i, and_term in enumerate(search_terms):
-    if i!=0:
+    if i != 0:
         add_term += '+AND+'
     add_term += '%28'
     for j, or_term in enumerate(and_term):
-        if j!=0:
+        if j != 0:
             add_term += '+OR+'
         add_term += or_term.replace(' ', '+')
 
@@ -78,12 +76,10 @@ for i, and_term in enumerate(search_terms):
     springer_url += add_term
     add_term = ''
 
-
 springer_url += '&showAll=true"'
 
 print("Springer url: " + springer_url)
 os.system('start chrome ' + springer_url)
-
 
 ############################################### SCOPUS
 
@@ -91,11 +87,11 @@ scopus_url = '"https://www.scopus.com/results/results.uri?sort=plf-f&src=s&s='
 
 add_term = '%28'
 for i, and_term in enumerate(search_terms):
-    if i!=0:
+    if i != 0:
         add_term += '+AND+'
     add_term += '%28'
     for j, or_term in enumerate(and_term):
-        if j!=0:
+        if j != 0:
             add_term += '+OR+'
         add_term += 'ALL%28%22' + or_term + '%22%29'
 
@@ -103,14 +99,11 @@ for i, and_term in enumerate(search_terms):
     scopus_url += add_term
     add_term = ''
 
-
-scopus_url += '%29+AND+PUBYEAR+%26gt%3B+' + years[0] + '+AND+PUBYEAR+%26lt%3B+' + str(int(years[1])+1) + '&origin=searchadvanced"'
+scopus_url += '%29+AND+PUBYEAR+%26gt%3B+' + years[0] + '+AND+PUBYEAR+%26lt%3B+' + str(
+    int(years[1]) + 1) + '&origin=searchadvanced"'
 
 print("Springer url: " + scopus_url)
 os.system('start chrome ' + scopus_url)
-
-
-
 
 ############################################### WILEY
 
@@ -118,11 +111,11 @@ wiley_url = '"https://onlinelibrary.wiley.com/action/doSearch?field1=AllField&te
 
 add_term = '%28'
 for i, and_term in enumerate(search_terms):
-    if i!=0:
+    if i != 0:
         add_term += '+AND+'
     add_term += '%28'
     for j, or_term in enumerate(and_term):
-        if j!=0:
+        if j != 0:
             add_term += '+OR+'
         add_term += or_term
 
@@ -130,14 +123,11 @@ for i, and_term in enumerate(search_terms):
     wiley_url += add_term
     add_term = ''
 
-
-wiley_url += '%29&field2=AllField&text2=&field3=AllField&text3=&Ppub=&AfterMonth=1&AfterYear=' + years[0] + '&BeforeMonth=12&BeforeYear=' + years[1] + '"'
+wiley_url += '%29&field2=AllField&text2=&field3=AllField&text3=&Ppub=&AfterMonth=1&AfterYear=' + years[
+    0] + '&BeforeMonth=12&BeforeYear=' + years[1] + '"'
 
 print("Wiley url: " + wiley_url)
 os.system('start chrome ' + wiley_url)
-
-
-
 
 ############################################### IET DL
 
@@ -145,11 +135,11 @@ iet_url = '"https://digital-library.theiet.org/search?value1='
 
 add_term = ''
 for i, and_term in enumerate(search_terms):
-    if i!=0:
+    if i != 0:
         add_term += '+AND+'
     add_term += '%28'
     for j, or_term in enumerate(and_term):
-        if j!=0:
+        if j != 0:
             add_term += '+OR+'
         add_term += or_term.replace(' ', '+')
 
@@ -157,12 +147,14 @@ for i, and_term in enumerate(search_terms):
     iet_url += add_term
     add_term = ''
 
-
-iet_url += '&fulltextcheck=option1checked&option1=all&operator3=AND&value3=&option3=author&operator2=AND&value2=&option2=title&operator4=AND&value4=&option4=issnisbndoi&operator5=NOT&value5=&option5=all&operator7=AND&option7=subjectarea&value7=&operator6=AND&option6=contentType&value6=&maxyear=' + years[1] + '&operator9=AND&option9=year_from&value9=' + years[0] + '&operator10=AND&option10=year_to&value10=' + years[1] + '&sortField=default&sortDescending=true&pageSize=10"'
+iet_url += '&fulltextcheck=option1checked&option1=all&operator3=AND&value3=&option3=author&operator2=AND&value2' \
+           '=&option2=title&operator4=AND&value4=&option4=issnisbndoi&operator5=NOT&value5=&option5=all&operator7=' \
+           'AND&option7=subjectarea&value7=&operator6=AND&option6=contentType&value6=&maxyear=' + years[1] + \
+           '&operator9=AND&option9=year_from&value9=' + years[0] + '&operator10=AND&option10=year_to&value10=' + \
+           years[1] + '&sortField=default&sortDescending=true&pageSize=10"'
 
 print("IET DL url: " + iet_url)
 os.system('start chrome ' + iet_url)
-
 
 ############################################### DBPL
 
@@ -170,18 +162,17 @@ dblp_url = '"https://dblp.org/search?q='
 
 add_term = ''
 for i, and_term in enumerate(search_terms):
-    if i!=0:
+    if i != 0:
         add_term += '+%20'
     add_term += '%28'
     for j, or_term in enumerate(and_term):
-        if j!=0:
+        if j != 0:
             add_term += '%7C'
         add_term += or_term.replace(' ', '+%20')
 
     add_term += '%29'
     dblp_url += add_term
     add_term = ''
-
 
 dblp_url += '"'
 
